945d92cc9c5ef901a06fc7c992071989789d76ff
AaronTYin <1355137560@qq.com>
2020-02-28 21:31:34 +0800

    200228

---
+++ ATY_CPP/5_2/5_2Test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+#include "5_2.h"
+
+int main()
+{
+	Client a;
+	a.get();
+	a.show();
+	return 0;
+}
---
+++ ATY_CPP/6_6/6_6.cpp
@@ -0,0 +1,9 @@
+#include <iostream.h>
+#include "6_6.h"
+
+main()
+{
+	int i;
+	Employee emp[5]=;
+	for
+}
